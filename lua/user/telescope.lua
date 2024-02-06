ff26ab14d72ee6c4fc61d7220709500999fc5712
antsoh161 <soderlund.anton@gmail.com>
2024-02-06 20:24:30 +0100

    add fzf-native as fuzzy finder

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,50 +1,38 @@
-local M = {
-	"nvim-telescope/telescope.nvim",
-	event = "Bufenter",
-	cmd = { "Telescope" },
-	dependencies = {
-		{
-			"nvim-lua/plenary.nvim",
-		},
-	},
+return {
+   "nvim-telescope/telescope.nvim",
+   event = "Bufenter",
+   cmd = { "Telescope" },
+   dependencies = {
+      {
+         "nvim-lua/plenary.nvim",
+      },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+   },
+
+   config = function()
+      local actions = require("telescope.actions")
+      local layout = require("telescope.actions.layout")
+      require("telescope").setup({
+         defaults = {
+            prompt_prefix = " ",
+            selection_caret = " ",
+            path_display = { "truncate" },
+            file_ignore_patterns = { ".git/", "node_modules" },
+
+            preview = {
+               hide_on_startup = false,
+            },
+            mappings = {
+               i = {
+                  ["<Down>"] = actions.cycle_history_next,
+                  ["<Up>"] = actions.cycle_history_prev,
+                  ["<C-j>"] = actions.move_selection_next,
+                  ["<C-k>"] = actions.move_selection_previous,
+                  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                  ["<C-p>"] = layout.toggle_preview,
+               },
+            },
+         },
+      })
+   end,
 }
-
-function M.config()
-	local actions = require("telescope.actions")
-	local layout = require("telescope.actions.layout")
-	require("telescope").setup({
-		defaults = {
-			prompt_prefix = " ",
-			selection_caret = " ",
-			path_display = { "truncate" },
-			file_ignore_patterns = { ".git/", "node_modules" },
-
-			preview = {
-				hide_on_startup = false,
-			},
-			mappings = {
-				i = {
-					["<Down>"] = actions.cycle_history_next,
-					["<Up>"] = actions.cycle_history_prev,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-k>"] = actions.move_selection_previous,
-					["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					["<C-p>"] = layout.toggle_preview,
-				},
-			},
-		},
-		-- pickers = {
-		-- find_files = {
-		--   hidden = true,
-		--   find_command = {
-		--     'rg',
-		--     '--files',
-		--     '--color',
-		--     'never',
-		--   },
-		-- },
-		-- },
-	})
-end
-
-return M
