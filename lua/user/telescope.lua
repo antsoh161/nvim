95d861dd54eb50c674fbb25d24a47b9d77d6f48f
antsoh161 <soderlund.anton@gmail.com>
2024-10-24 19:23:08 +0200

    bugfix: Activate fzf for telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,38 +1,45 @@
 return {
-   "nvim-telescope/telescope.nvim",
-   event = "Bufenter",
-   cmd = { "Telescope" },
-   dependencies = {
-      {
-         "nvim-lua/plenary.nvim",
-      },
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-   },
+	"nvim-telescope/telescope.nvim",
+	event = "Bufenter",
+	cmd = { "Telescope" },
+	dependencies = {
+		{
+			"nvim-lua/plenary.nvim",
+		},
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 
-   config = function()
-      local actions = require("telescope.actions")
-      local layout = require("telescope.actions.layout")
-      require("telescope").setup({
-         defaults = {
-            prompt_prefix = " ",
-            selection_caret = " ",
-            path_display = { "truncate" },
-            file_ignore_patterns = { ".git/", "node_modules" },
+	config = function()
+		local actions = require("telescope.actions")
+		local layout = require("telescope.actions.layout")
+		require("telescope").setup({
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+				path_display = { "truncate" },
+				file_ignore_patterns = { ".git/", "node_modules" },
 
-            preview = {
-               hide_on_startup = false,
-            },
-            mappings = {
-               i = {
-                  ["<Down>"] = actions.cycle_history_next,
-                  ["<Up>"] = actions.cycle_history_prev,
-                  ["<C-j>"] = actions.move_selection_next,
-                  ["<C-k>"] = actions.move_selection_previous,
-                  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                  ["<C-p>"] = layout.toggle_preview,
-               },
-            },
-         },
-      })
-   end,
+				preview = {
+					hide_on_startup = false,
+				},
+				mappings = {
+					i = {
+						["<Down>"] = actions.cycle_history_next,
+						["<Up>"] = actions.cycle_history_prev,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+		require("telescope").load_extension("fzf")
+	end,
 }
