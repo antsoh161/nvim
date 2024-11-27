7a68489d2bcf85d35c67b81208ddfce9821c464c
antsoh161 <soderlund.anton@gmail.com>
2024-11-27 20:51:21 +0100

    fix: stop noice from so annoicing

--- lua/user/noice.lua
+++ lua/user/noice.lua
@@ -1,22 +1,64 @@
 return {
-   "folke/noice.nvim",
-   -- event = "VeryLazy",
-   dependencies = {
-      "MunifTanjim/nui.nvim",
-      -- "rcarriga/nvim-notify",
-   },
-   config = function()
-      require("noice").setup({
-         lsp = {
-            override = {
-               ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-               ["vim.lsp.util.stylize_markdown"] = true,
-               ["cmp.entry.get_documentation"] = true,
-            },
-         },
-         cmdline = {
-            view = "cmdline",
-         },
+ "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.debug = false
+      opts.routes = opts.routes or {}
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
       })
-   end,
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+
+      table.insert(opts.routes, 1, {
+        filter = {
+          ["not"] = {
+            event = "lsp",
+            kind = "progress",
+          },
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+      return opts
+    end,
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+    opts = {
+      preset = "helix",
+      debug = vim.uv.cwd():find("which%-key"),
+      win = {},
+      spec = {},
+    },
+  },
+  {
+    "j-hui/fidget.nvim",
+    event = "LspAttach",
+    opts = {},
+  }, -- LSP progress
 }
