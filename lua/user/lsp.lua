876d99d1701e88b92279c9de41536bbadef1d02f
Anton Soderlund <anton.soderlund@epiroc.com>
2024-11-28 17:23:36 +0100

    feat: toggleable inlay-hints

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,108 +1,121 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = "BufReadPre",
-	cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
-	opts = { inlay_hints = true },
+   "neovim/nvim-lspconfig",
+   event = "BufReadPre",
+   cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
+   opts = { inlay_hints = true },
 
-	config = function()
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+   config = function()
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.completion.completionItem.snippetSupport = true
+   --    local capabilities = vim.lsp.protocol.make_client_capabilities()
+   -- 
+   --    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
-		capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-		capabilities.offsetEncoding = { "utf-16" }
+      local capabilities = vim.tbl_deep_extend("force",
+         vim.lsp.protocol.make_client_capabilities(),
+         cmp_nvim_lsp.default_capabilities())
+      capabilities.offsetEncoding = { "utf-16" }
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
-		local function opts(bufnr, desc)
-			return { buffer = bufnr, desc = "LSP " .. desc }
-		end
+      local function opts(bufnr, desc)
+         return { buffer = bufnr, desc = "LSP " .. desc }
+      end
 
-		local function lsp_keymaps(bufnr)
-			local keymap = vim.keymap.set
-			keymap("n", "gD", vim.lsp.buf.declaration, opts(bufnr, "Go to declaration"))
-			keymap("n", "gd", vim.lsp.buf.definition, opts(bufnr, "Go to definition"))
-			keymap("n", "<C-Space>", vim.lsp.buf.hover, opts(bufnr, "Hover"))
-			keymap("n", "gI", vim.lsp.buf.implementation, opts(bufnr, "Go to implementation"))
-			keymap("n", "gr", vim.lsp.buf.references, opts(bufnr, "Go to references"))
-			keymap("n", "gl", vim.diagnostic.open_float, opts(bufnr, "Open diagnostic"))
-			keymap("n", "<leader>la", vim.lsp.buf.code_action, opts(bufnr, "Do code action"))
-			keymap("n", "<leader>lr", vim.lsp.buf.rename, opts(bufnr, "Rename"))
-			keymap("n", "<leader>ls", vim.lsp.buf.signature_help, opts(bufnr, "Signature"))
-			keymap("n", "<leader>lq", vim.diagnostic.setloclist, opts(bufnr, "Set loclist"))
+      local function lsp_keymaps(client, bufnr)
+         local function toggle_hints()
+            if vim.g.inlay_hints_visible then
+               vim.g.inlay_hints_visible = false
+               vim.lsp.inlay_hint.enable(false, { bufnr })
+            else
+               if client.server_capabilities.inlayHintProvider then
+                  vim.g.inlay_hints_visible = true
+                  vim.lsp.inlay_hint.enable(true, { bufnr })
+               end
+            end
+         end
+         local keymap = vim.keymap.set
+         keymap("n", "gD", vim.lsp.buf.declaration, opts(bufnr, "Go to declaration"))
+         keymap("n", "gd", vim.lsp.buf.definition, opts(bufnr, "Go to definition"))
+         keymap("n", "<C-Space>", vim.lsp.buf.hover, opts(bufnr, "Hover"))
+         keymap("n", "gI", vim.lsp.buf.implementation, opts(bufnr, "Go to implementation"))
+         keymap("n", "gr", vim.lsp.buf.references, opts(bufnr, "Go to references"))
+         keymap("n", "gl", vim.diagnostic.open_float, opts(bufnr, "Open diagnostic"))
+         keymap("n", "<leader>la", vim.lsp.buf.code_action, opts(bufnr, "Do code action"))
+         keymap("n", "<leader>lr", vim.lsp.buf.rename, opts(bufnr, "Rename"))
+         keymap("n", "<leader>ls", vim.lsp.buf.signature_help, opts(bufnr, "Signature"))
+         keymap("n", "<leader>lq", vim.diagnostic.setloclist, opts(bufnr, "Set loclist"))
 
-			keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.jump({ count = 1 })<CR>", opts(bufnr, "Jump to next"))
-			keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.jump({ count = -1 })<CR>", opts(bufnr, "Jump to prev"))
-		end
+         keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.jump({ count = 1 })<CR>", opts(bufnr, "Jump to next"))
+         keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.jump({ count = -1 })<CR>", opts(bufnr, "Jump to prev"))
+         keymap("n", "<leader>lt", toggle_hints, opts(bufnr, "Toggle inlay hints"))
+      end
 
-		local lspconfig = require("lspconfig")
-		local on_attach = function(client, bufnr)
-			lsp_keymaps(bufnr)
-			-- require("illuminate").on_attach(client)
-			if client.server_capabilities.inlayHintProvider then
-				-- vim.g.inlay_hints_visible = true
-				-- vim.lsp.inlay_hint.enable(true, { bufnr })
-			end
-		end
+      local lspconfig = require("lspconfig")
 
-		local on_init = function(client, _)
-			if client.supports_method("textDocument/semanticTokens") then
-				client.server_capabilities.semanticTokensProvider = nil
-			end
-		end
+      local on_attach = function(client, bufnr)
+         lsp_keymaps(client,bufnr)
+      end
 
-		for _, server in pairs(require("shared.lsp").lsp_servers) do
-			local lsp_opts = {
-				on_attach = on_attach,
-				capabilities = capabilities,
-				on_init = on_init,
-			}
+      local on_init = function(client, _)
+         if client:supports_method("textDocument/semanticTokens") then
+            client.server_capabilities.semanticTokensProvider = nil
+         end
+      end
 
-			server = vim.split(server, "@")[1]
+      for _, server in pairs(require("shared.lsp").lsp_servers) do
+         local lsp_opts = {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            on_init = on_init,
+         }
 
-			local require_ok, conf_opts = pcall(require, "shared.lsp_settings." .. server)
-			if require_ok then
-				lsp_opts = vim.tbl_deep_extend("error", conf_opts, lsp_opts)
-			end
-			lspconfig[server].setup(lsp_opts)
-		end
+         server = vim.split(server, "@")[1]
 
-		local signs = {
-			{ name = "DiagnosticSignError", text = "" },
-			{ name = "DiagnosticSignWarn", text = "" },
-			{ name = "DiagnosticSignHint", text = "" },
-			{ name = "DiagnosticSignInfo", text = "" },
-		}
+         local require_ok, conf_opts = pcall(require, "shared.lsp_settings." .. server)
+         if require_ok then
+            lsp_opts = vim.tbl_deep_extend("error", conf_opts, lsp_opts)
+         end
+         lspconfig[server].setup(lsp_opts)
+      end
 
-		for _, sign in ipairs(signs) do
-			vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-		end
+      local signs = {
+         { name = "DiagnosticSignError", text = "" },
+         { name = "DiagnosticSignWarn", text = "" },
+         { name = "DiagnosticSignHint", text = "" },
+         { name = "DiagnosticSignInfo", text = "" },
+      }
 
-		local config = {
-			-- disable virtual text
-			virtual_text = { true },
-			signs = false,
-			update_in_insert = true,
-			underline = true,
-			severity_sort = true,
-			float = {
-				focusable = false,
-				style = "minimal",
-				border = "rounded",
-				source = "always",
-				header = "",
-				prefix = "",
-				suffix = "",
-			},
-		}
+      for _, sign in ipairs(signs) do
+         vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+      end
 
-		vim.diagnostic.config(config)
+      local config = {
+         -- disable virtual text
+         virtual_text = { true },
+         signs = false,
+         update_in_insert = true,
+         underline = true,
+         severity_sort = true,
+         float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+            suffix = "",
+         },
+      }
 
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-			border = "rounded",
-		})
+      vim.diagnostic.config(config)
 
-		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-			border = "rounded",
-		})
-	end,
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+         border = "rounded",
+      })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+         border = "rounded",
+      })
+   end,
 }
