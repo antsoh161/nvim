acad0aabb35458d8152b23168dc54abd921ba65e
antsoh161 <soderlund.anton@gmail.com>
2024-12-21 15:55:10 +0100

    feat: Revise copilto settings

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,29 +1,35 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		build = ":Copilot auth",
-		event = "InsertEnter",
-		opts = {
-			suggestion = {
-				enabled = true,
+   {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      event = "InsertEnter",
+      opts = {
+         suggestion = {
+            enabled = true,
             auto_trigger = true,
             hide_during_completion = true,
-			},
-			panel = {
-				enabled = false,
-			},
-		},
-		config = function(opts)
-			require("copilot").setup(opts)
-		end,
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		build = "make tiktoken",
-		opts = { show_help = false },
-		config = function(opts)
-			require("CopilotChat").setup(opts)
-		end,
-	},
+         },
+         panel = {
+            enabled = false,
+         },
+      },
+      config = function(opts)
+         require("copilot").setup(opts)
+      end,
+   },
+   {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      build = "make tiktoken",
+      config = function()
+         require("CopilotChat").setup({
+            mappings = {
+               reset = {
+                  normal = '',
+                  insert = '',
+               },
+            },
+         })
+      end,
+   },
 }
