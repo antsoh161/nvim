d3ec2e45d11cb8539d226f90e87c6fd0fc4a4a24
antsoh161 <soderlund.anton@gmail.com>
2026-03-14 14:03:47 +0100

    fix: pyright lsp config has bad string

--- lua/config/lsp_settings/pyright.lua
+++ lua/config/lsp_settings/pyright.lua
@@ -1,7 +1,7 @@
 local pyright_cmd = vim.fn.glob("~/.local/share/nvim/mason/bin/basedpyright-langserver")
 return {
    cmd = { pyright_cmd, "--stdio" },
-   filetypes = { "python " },
+   filetypes = { "python" },
    root_markers = {
       "pyrightconfig.json",
       "pyproject.toml",
